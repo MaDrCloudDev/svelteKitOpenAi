1a157f75d312af5f20b91ab86d149602775ecef9
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2023-03-01 13:36:29 -0700

    update for continuity and release
    update for continuity and release

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -13,49 +13,44 @@
 </script>
 
 <header class="sticky top-0 z-50 bg-black text-[#7f7d76]">
-	<div class="pt-2 pb-2 flex-grow max-w-lg mx-auto">
+	<div class="pt-2 pb-2 flex-grow max-w-2xl mx-auto">
 		<ul class="flex items-center justify-between">
 			<li class="ml-2">
-				<a on:click={toggle} href="https://madr.io/" aria-label="MaDr Logo">
+				<a on:click={toggle} href="/" aria-label="MaDr Logo">
 					<LogoNav />
 				</a>
 			</li>
-			<div class="w-full justify-around max-w-md mx-auto text-xl hidden md:flex">
+			<div class="w-full justify-around max-w-2xl mx-auto text-xl hidden md:flex">
 				<li class="hover:text-[#fffb00]">
-					<a
-						class:active={$page.url.pathname == 'https://madr.io/about/'}
-						href="https://madr.io/about/">About</a
+					<a class:active={$page.url.pathname == '/about'} href="https://madr.io/about/">About</a>
+				</li>
+				<li class="hover:text-[#fffb00]">
+					<a class:active={$page.url.pathname == '/tech'} href="https://madr.io/tech/">Tech</a>
+				</li>
+				<li class="hover:text-[#fffb00]">
+					<a class:active={$page.url.pathname == '/projects'} href="https://madr.io/projects/"
+						>Projects</a
 					>
 				</li>
 				<li class="hover:text-[#fffb00]">
-					<a
-						class:active={$page.url.pathname == 'https://madr.io/tech/'}
-						href="https://madr.io/tech/">Tech</a
+					<a class:active={$page.url.pathname == '/weather'} href="https://madr.io/weather/"
+						>Weather?</a
 					>
 				</li>
 				<li class="hover:text-[#fffb00]">
-					<a
-						class:active={$page.url.pathname == 'https://madr.io/projects/'}
-						href="https://madr.io/projects/">Projects</a
-					>
+					<a class:active={$page.url.pathname == '/'} href="/">Robot</a>
 				</li>
 				<li class="hover:text-[#fffb00]">
-					<a
-						class:active={$page.url.pathname == 'https://madr.io/contact/'}
-						href="https://madr.io/contact/">Contact</a
+					<a class:active={$page.url.pathname == '/contact'} href="https://madr.io/contact"
+						>Contact</a
 					>
 				</li>
+				<!-- <li class="hover:text-[#fffb00]">
+				<a class:active={$page.url.pathname == '/guestbook'} href="/guestbook">Guestbook</a>
+				</li> -->
+
 				<li class="hover:text-[#fffb00]">
-					<a
-						class:active={$page.url.pathname == 'https://madr.io/weather/'}
-						href="https://madr.io/weather/">Weather?</a
-					>
-				</li>
-				<li class="hover:text-[#fffb00]">
-					<a
-						class:active={$page.url.pathname == 'https://madr.io/login/'}
-						href="https://madr.io/login/">Login</a
-					>
+					<a class:active={$page.url.pathname == '/login'} href="https://madr.io/login">Login</a>
 				</li>
 			</div>
 			<li>
