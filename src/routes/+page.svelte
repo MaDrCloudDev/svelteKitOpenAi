a9c1a62aff62d6c791626d40646681fdc9dba707
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2023-02-28 18:58:13 -0700

    updated input to work on mobile
    updated input to work on mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -58,18 +58,21 @@
 </svelte:head>
 
 <h1 class="text-4xl font-bold text-[#328eef] text-center">flirtatiousBot</h1>
-<form
-	class="flex flex-col pt-8 max-w-lg w-full mx-auto"
-	on:submit|preventDefault={() => handleSubmit()}
->
+<form class="flex flex-col pt-8 max-w-lg w-full mx-auto" on:submit|preventDefault={handleSubmit}>
 	<label class="text-[#fffb00] text-xl mb-1" for="context">Introduce yourself to the bot:</label>
-	<textarea class="bg-black border text-[#328eef]" name="context" rows="5" bind:value={context} />
+	<input
+		placeholder="name/age/location/etc..."
+		class="bg-black border text-[#328eef] p-4"
+		name="context"
+		bind:value={context}
+	/>
 	<button
+		type="submit"
 		class="btn btn-black w-fit mx-auto mt-3 p-4 rounded-full border border-gray-900 text-[#7f7d76] shadow-md shadow-gray-700 hover:scale-110 ease-in duration-200 hover:text-[#fffb00] hover:border-[#328eef] hover:shadow-[#328eef] text-sm md:text-lg"
 		>Introduce Yourself</button
 	>
 	<div class="pt-4">
-		<h2 class="text-2xl font-bold">flirtatiousBot:</h2>
+		<h2 class="text-2xl font-bold text-right">flirtatiousBot:</h2>
 		{#if answer}
 			<p>{answer}</p>
 		{/if}
