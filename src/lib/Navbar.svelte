bca9ce87cfb3935fefdc461003b2130c79968ba5
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2023-04-27 13:49:34 -0700

    update nav and sidebar to match main site
    update nav and sidebar to match main site

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -12,21 +12,21 @@
 	}
 </script>
 
-<ul
+<nav
 	style="background-color: hsla(0, 0%, 0%, 0.5)"
-	class="px-3 mt-2 flex justify-between sticky top-0 z-50 backdrop-blur-sm max-w-2xl sm:max-w-4xl mx-auto"
+	class="px-3 mt-2 flex justify-between sticky top-0 z-50 backdrop-blur-sm max-w-2xl sm:max-w-4xl mx-auto mb-2 sm:mb-0"
 >
-	<div class="hidden md:flex w-full justify-between items-center text-xl">
-		<a on:click={toggle} href="https://madr.io/" aria-label="MaDr Logo"><li><LogoNavBw /></li></a>
-		<a class:active={$page.url.pathname == '/about'} href="https://madr.io/about"><li>about</li></a>
-		<a class:active={$page.url.pathname == '/tech'} href="https://madr.io/tech"><li>tech</li></a>
-		<a class:active={$page.url.pathname == '/projects'} href="https://madr.io/projects"><li>projects</li></a>
-		<a class:active={$page.url.pathname == '/weather'} href="https://madr.io/weather"><li>weather?</li></a>
-		<a class:active={$page.url.pathname == '/'} href="https://flirtatiousai.madr.io/"><li>
+	<section class="hidden md:flex w-full gap-20 items-center text-xl">
+		<item><a on:click={toggle} href="https://madr.io/" aria-label="MaDr Logo"><LogoNavBw /></a></item>
+		<item><a class:active={$page.url.pathname == '/about'} href="https://madr.io/about">about</a></item>
+		<item><a class:active={$page.url.pathname == '/tech'} href="https://madr.io/tech">tech</a></item>
+		<item><a class:active={$page.url.pathname == '/projects'} href="https://madr.io/projects">projects</a></item>
+		<item><a class:active={$page.url.pathname == '/weather'} href="https://madr.io/weather">weather?</a></item>
+		<item><a class:active={$page.url.pathname == '/'} href="https://flirtatiousai.madr.io/">
 			ai?
-		</li></a>
-		<a class:active={$page.url.pathname == '/contact'} href="https://madr.io/contact"><li>contact</li></a>
-	</div>
+		</a></item>
+		<item><a class:active={$page.url.pathname == '/contact'} href="https://madr.io/contact">contact</a></item>
+	</section>
 	<div />
-	<li class=""><Hamburger bind:open={sidebar} /></li>
-</ul>
+	<Hamburger bind:open={sidebar} />
+</nav>
--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -9,49 +9,48 @@
 	console.log($page.url.pathname)
 </script>
 
-<aside class="fixed w-full h-full bg-black shadow-lg text-3xl text-center z-50" class:open>
-	<ul class="flex flex-col gap-6 content">
-		<a on:click={toggle} class:active={$page.url.pathname == '/about'} href="https://madr.io/about/"
-			><li class="hover:text-[#fffb00] mb-3">about</li></a
-		>
-		<a
+<menu class="fixed w-full h-full bg-black shadow-lg text-3xl text-center z-50" class:open>
+	<section class="flex flex-col gap-6 content">
+		<item class="hover:text-[#fffb00] mb-3"><a on:click={toggle} class:active={$page.url.pathname == '/about'} href="https://madr.io/about/"
+			>about</a
+		></item>
+		<item class="hover:text-[#fffb00] mb-3"><a
 			on:click={toggle}
 			class:active={$page.url.pathname == 'https://madr.io/tech/'}
-			href="https://madr.io/tech/"><li class="hover:text-[#fffb00] mb-3">tech</li></a
-		>
-		<a
+			href="https://madr.io/tech/"><item class="hover:text-[#fffb00] mb-3">tech</a
+		></item>
+		<item class="hover:text-[#fffb00] mb-3"><a
 			on:click={toggle}
 			class:active={$page.url.pathname == 'https://madr.io/projects/'}
-			href="https://madr.io/projects/"><li class="hover:text-[#fffb00] mb-3">projects</li></a
-		>
-		<a
+			href="https://madr.io/projects/"><item class="hover:text-[#fffb00] mb-3">projects</a
+		></item>
+		<item class="hover:text-[#fffb00] mb-3"><a
 			on:click={toggle}
 			class:active={$page.url.pathname == 'https://madr.io/weather/'}
-			href="https://madr.io/weather/"><li class="hover:text-[#fffb00] mb-3">weather?</li></a
-		>
-		<a class:active={$page.url.pathname == '/'} href="https://flirtatiousai.madr.io/"
-			><li class="hover:text-[#fffb00]">ai?</li></a
-		>
-		<a
+			href="https://madr.io/weather/"><item class="hover:text-[#fffb00] mb-3">weather?</a
+		></item>
+		<item class="hover:text-[#fffb00] mb-3"><a class:active={$page.url.pathname == '/'} href="https://flirtatiousai.madr.io/"
+			><item class="hover:text-[#fffb00]">ai?</a
+		></item>
+		<item class="hover:text-[#fffb00] mb-3"><a
 			on:click={toggle}
 			class:active={$page.url.pathname == 'https://madr.io/contact/'}
-			href="https://madr.io/contact/"><li class="hover:text-[#fffb00] mb-3">contact</li></a
-		>
-		<div class="mx-auto">
+			href="https://madr.io/contact/"><item class="hover:text-[#fffb00] mb-3">contact</a
+		></item>
+		<item class="mx-auto">
 			<a
 				on:click={toggle}
 				class:active={$page.url.pathname == 'https://madr.io/'}
 				href="https://madr.io/"
-				><li>
+				>
 					<LogoBW />
-				</li></a
-			>
-		</div>
-	</ul>
-</aside>
+				</a
+			></item>
+	</section>
+</menu>
 
 <style>
-	aside {
+	menu {
 		left: -100%;
 		transition: left 0.3s ease-in-out;
 	}
